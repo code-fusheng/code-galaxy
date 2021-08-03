47de3b52c560b61f615b5a26ac3b04cbb87382b9
code-fusheng <2561035977@qq.com>
2021-08-04 00:33:37 +0800

    fix

--- docker/batch-run-prod.sh
+++ docker/batch-run-prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker run -it -m 512M -d -p 9999:9999 --rm gateway-server
-docker run -it -m 256M -d -p 9000:9000 --rm auth-server
+docker run -it -m 512M -d -p 9000:9000 --rm auth-server
 docker run -it -m 256M -d -p 10000:10000 --rm sys-server
 docker run -it -m 256M -d -p 10100:10100 --rm user-server
 docker run -it -m 256M -d -p 10200:10200 --rm article-server
